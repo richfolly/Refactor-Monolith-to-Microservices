511b60c618817b681ed097d3f0643620e0ea9048
seamlessOlanrewaju <olanrewaju@seamlesshr.com>
2021-02-13 18:39:46 +0100

    feat/Refactor Monolith to Microservices

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=username
+export POSTGRES_PASSWORD=password
+export POSTGRES_HOST=third-project.cdivd2oc1rwt.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=third-project-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=profile
+export JWT_SECRET=hello
+export URL=http://localhost:8100
